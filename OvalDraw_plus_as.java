b91fffe6a5a8def1d636996645ed92d79235aa91
Blazingshadow10 <61059862+Blazingshadow10@users.noreply.github.com>
2022-02-06 18:12:04 -0600

    Update OvalDraw_plus_as.java

--- OvalDraw_plus_as.java
+++ OvalDraw_plus_as.java
@@ -8,46 +8,13 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Container; 
 import java.awt.Color;
-import java.util.Random;
 
 // Decides the color of the shapes
 class shapeColor extends JPanel {
 
-    private Color ovalColor;
+    private Color ovalColor = new Color(25,200,0);
     private Color rectColor = new Color(25,25,112);
-
-    public void setColorOval(int red, int green, int blue) {
-        ovalColor = new Color(red, green, blue);
-    }
     
-    // Random picks the color
-    shapeColor() {
-        int r = 0,g = 0,b = 0;
-        Random rand = new Random();
-        int randomColors = rand.nextInt(3);
-
-        // 
-        if (randomColors == 0) 
-        {
-            r = 0;
-            g = 255;
-            b = 0;
-        } 
-        else if (randomColors == 1)
-        {
-            r = 255;
-            g = 0;
-            b = 0;
-        }
-        else if (randomColors == 2)
-        {
-            r = 0;
-            g = 0;
-            b = 0;
-        }
-        setColorOval(r, g, b);
-    }
-
     // Drawing the shapes to there sizes
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
